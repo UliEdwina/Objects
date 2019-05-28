d6b31835d4f3abcf403d250ffb4435a78bde9a48
UliEdwina <uli.orduno@codeimmersives.com>
2019-05-28 14:16:27 -0400

    first commit

---
+++ school.js
@@ -0,0 +1,12 @@
+
+
+
+function OurSchool(branch, term, students){
+    this.branch = branch
+    this.term = term
+    this.students = students
+
+}
+ let school = new OurSchool("Code Immersives", "Term Two", "Uli")
+
+console.log(`school: `, school)
